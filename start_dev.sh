354ba32e446b75a49bb5b7c482b524ba78d09dc0
LorenzoDag <lorenzo-dagostino@libero.it>
2022-06-03 19:49:23 +0200

    modificato init_db, tolto Dockerfile e montato il volume (così non serve ribuildare ogni volta), eliminato start_dev script e aggiunto .env file (docker-compose up vede già l'env)

--- start_dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export MONGODB_ADMINUSERNAME="admin"
-export MONGODB_ADMINPASSWORD="admin"
-export MONGODB_SERVER="localhost"
-export MONGODB_PORT="27017"
-export MONGODB_DATABASE="project"
-
-docker-compose up &
